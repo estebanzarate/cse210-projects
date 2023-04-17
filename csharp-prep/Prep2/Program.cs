cad229f175e709b098288219f3c5ac9ea0197435
estebanzarate <estebanzaratedev@gmail.com>
2023-04-17 19:02:32 -0300

    Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.Write("Grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+        if (grade >= 90) letter = "A";
+        else if (grade >= 80) letter = "B";
+        else if (grade >= 70) letter = "C";
+        else if (grade >= 60) letter = "D";
+        else letter = "F";
+        Console.WriteLine($"Grade: {letter}");
+        if (grade >= 70) Console.WriteLine("Congratulations..!!");
+        else Console.WriteLine("Try next time..!!");
     }
 }
