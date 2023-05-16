f4db831c6bc17e9d46f1f98bff27b9cd4dfae553
estebanzarate <estebanzaratedev@gmail.com>
2023-05-15 22:52:12 -0300

    learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(222);
+        Fraction fraction3 = new Fraction(333, 666);
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction1.GetBottom());
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Console.WriteLine(fraction2.GetTop());
+        Console.WriteLine(fraction2.GetBottom());
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetTop());
+        Console.WriteLine(fraction3.GetBottom());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
